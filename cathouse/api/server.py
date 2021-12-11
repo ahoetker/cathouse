812a0b666b331e96edbc1542adb52264b90bbc23
Andrew Hoetker <andrew@hoetker.engineer>
2021-12-11 16:33:41 -0700

    add dashboard

--- cathouse/api/server.py
+++ cathouse/api/server.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from cathouse.api.routes import router as api_router
+from cathouse.core import tasks
 from cathouse.api.dependencies.settings import (
     get_settings,
 )  # pytest
-from cathouse.api.routes import router as api_router
-from cathouse.core import tasks
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 
 def get_application():
@@ -19,6 +23,14 @@ def get_application():
     app.add_event_handler("startup", tasks.create_start_app_handler(app))
     app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
     app.include_router(api_router, prefix="")
+
+    app.mount("/static", StaticFiles(directory="cathouse/static"), name="static")
+    templates = Jinja2Templates(directory="cathouse/templates")
+
+    @app.get("/", response_class=HTMLResponse)
+    async def index(request: Request):
+        return templates.TemplateResponse("index.html", {"request": request})
+
     return app
 
 
